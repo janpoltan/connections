349f1d8fab30ac8d231c385bd79eeeba8630f3a2
John Paul Rabanal <johnpaul@zilverband.com>
2017-09-22 13:00:06 +0800

    :sparkles: Added timeout when visiting profiles. Added simple storage for contacts using pickles

--- visit_connections.py
+++ visit_connections.py
@@ -4,6 +4,8 @@ Visit people you may know in LinkedIn
 """
 import time
 import sys
+import pickle
+import os
 from random import randint
 from selenium import webdriver
 from selenium.webdriver import ActionChains
@@ -12,20 +14,34 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+CONTACTS_FILE_PATH = 'contacts.p'
+
 driver = webdriver.Firefox()
 contacts_found = {}
+exclude_contacts = []
 
-def do_visit(username, password):
+def load_contacts():
+    if os.path.exists(CONTACTS_FILE_PATH):
+        contacts_found = pickle.load(open(CONTACTS_FILE_PATH, 'rb'))
+
+def save_contacts():
+    pickle.dump(contacts_found, open(CONTACTS_FILE_PATH, 'wb'))
+
+def do_visit(username, password, excluded):
     """Visits the profile under People you may know"""
     if username is None or password is None:
         sys.exit('username and pasword is require')
+
+    exclude_contacts = excluded
+    
+    load_contacts()
 
     do_login(username, password)
     
     go_to_mynetwork_page()
 
     # TODO: Way to check if the my network page was fully loaded
-    # Thus driver.get waits if the page was loaded ?
+    # Does driver.get waits if the page was loaded ?
     
     profile_list = driver.find_elements_by_class_name('mn-pymk-list__card')
     last_total_profiles = 0
@@ -48,8 +64,12 @@ def do_visit(username, password):
         else:
             last_total_profiles = total_profiles
             add_profile_to_visit(profile_list)
-    
+
     print 'Total profiles found: %s' % len(contacts_found)
+    visit_profiles()
+
+    save_contacts()
+    driver.close()
 
 
 def do_login(username, password):
@@ -86,6 +106,36 @@ def add_profile_to_visit(profile_list_elem):
                 u'profile_link': profile_link
             }
 
-def simulate_pause():
+def visit_profiles():
+    total_found = len(contacts_found)
+    if total_found > 0:
+        for key, value in contacts_found.iteritems():
+            contact_name = value.get('profile_name')
+            contact_link = value.get('profile_link')
+            try:
+                # TODO: Refactor this, proper way to exclude an item
+                if len(exclude_contacts) > 0:
+                    for ex in exclude_contacts:
+                        if contact_name.lower().find(ex) > -1:
+                            print 'Skipping ', contact_name
+                        else:
+                            visit_profile(contact_name, contact_link)
+                else:
+                    visit_profile(contact_name, contact_link)
+            except:
+                print "Unexpected error: ", sys.exc_info()[0]
+            finally:
+                ctr = ctr + 1
+                simulate_pause(start=5, end=15)
+
+
+def visit_profile(contact_name, contact_link):
+    print 'Visiting ', contact_name
+    driver.get(contact_link)
+    simulate_pause(start=1, end=4)
+    simulate_press_end()
+    simulate_pause(start=2, end=5)
+
+def simulate_pause(start=5, end=8):
     """Simulates a pause"""
-    time.sleep(randint(5, 8))
+    time.sleep(randint(start, end))
